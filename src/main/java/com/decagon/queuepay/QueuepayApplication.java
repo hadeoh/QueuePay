475e95c313939c96edf0daa5359585d85b39fe53
hadeoh <usthmandanfodio@gmail.com>
2020-03-03 13:06:09 -0800

    Initialize spring boot

---
+++ src/main/java/com/decagon/queuepay/QueuepayApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.queuepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuepayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuepayApplication.class, args);
+    }
+
+}
