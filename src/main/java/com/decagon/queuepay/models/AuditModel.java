b803ecb716f72cb6ba3a7f10041e5eb961c0b272
hadeoh <usthmandanfodio@gmail.com>
2020-03-03 13:57:57 -0800

    Add audit model

---
+++ src/main/java/com/decagon/queuepay/models/AuditModel.java
@@ -0,0 +1,34 @@
+package com.decagon.queuepay.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public abstract class AuditModel {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
