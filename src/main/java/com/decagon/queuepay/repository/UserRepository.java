16720401c45922e66cab5a63c8155d10430ca926
PiusAremieye <ebi44ril@gmail.com>
2020-03-03 17:06:30 +0100

    implemented the user details service class and override the loaduserbyusername method

--- src/main/java/com/decagon/queuepay/repository/UserRepository.java
+++ src/main/java/com/decagon/queuepay/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.decagon.queuepay.repository;
 import com.decagon.queuepay.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByEmail(String Email);
+    Optional<User> findByEmail(String Email);
 }
--- src/main/java/com/decagon/queuepay/service/MyUserDetailsService.java
+++ src/main/java/com/decagon/queuepay/service/MyUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.decagon.queuepay.service;
 
+import com.decagon.queuepay.models.MyUserDetails;
+import com.decagon.queuepay.models.User;
+import com.decagon.queuepay.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email " + email + " was not found"));
+        return MyUserDetails.build(user);
     }
 }
