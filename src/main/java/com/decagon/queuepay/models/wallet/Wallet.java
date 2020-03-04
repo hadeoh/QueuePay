ffc2f0012c5181b66ca10a974441252245067b66
hadeoh <usthmandanfodio@gmail.com>
2020-03-03 16:49:17 -0800

    Implement wallet model

--- src/main/java/com/decagon/queuepay/models/wallet/Wallet.java
+++ src/main/java/com/decagon/queuepay/models/wallet/Wallet.java
@@ -1,8 +1,7 @@
-package com.decagon.queuepay.models.transaction;
+package com.decagon.queuepay.models.wallet;
 
 import com.decagon.queuepay.models.AuditModel;
 import com.decagon.queuepay.models.Business;
-import com.decagon.queuepay.models.wallet.Wallet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,34 +19,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Transaction extends AuditModel {
+public class Wallet extends AuditModel {
 
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "walletId", nullable = false)
-    private Wallet wallet;
+    private Double balance = 0.00;
+
+    @NotNull
+    @NotBlank
+    private WalletType walletType;
+
+    @NotNull
+    @NotBlank
+    private String pin;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "businessId", nullable = false)
     private Business business;
-
-    private String customerName;
-
-    private CardType cardType;
-
-    @NotNull
-    @NotBlank
-    private Double amount;
-
-    @NotNull
-    @NotBlank
-    private TransactionStatus status;
-
-    @NotNull
-    @NotBlank
-    private TransactionType transactionType;
 }
---
+++ src/main/java/com/decagon/queuepay/models/wallet/WalletType.java
@@ -0,0 +1,5 @@
+package com.decagon.queuepay.models.wallet;
+
+public enum WalletType {
+    NAIRA, DOLLAR, GBP, EURO
+}
