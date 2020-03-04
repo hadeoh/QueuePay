4cf858c27d9fea26897c7da1997962e6b88f45e9
Demipo <41957499+Demipo@users.noreply.github.com>
2020-03-04 02:42:23 +0100

    Update CashOutService.java

---
+++ src/main/java/com/decagon/queuepay/models/CashOut.java
@@ -0,0 +1,68 @@
+package com.example.qpay.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties
+@Table(name = "cash_out")
+public class CashOut implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private double amount;
+    private String password;
+    private String bankName;
+    private String bankAccountName;
+
+    //SETTERS AND GETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankAccountName() {
+        return bankAccountName;
+    }
+
+    public void setBankAccountName(String bankAccountName) {
+        this.bankAccountName = bankAccountName;
+    }
+}
