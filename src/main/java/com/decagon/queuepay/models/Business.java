1d69d0a720604f217fa9d7f5dfb499c82b8e47af
hadeoh <usthmandanfodio@gmail.com>
2020-03-03 16:11:54 -0800

    Implement business model

--- src/main/java/com/decagon/queuepay/models/Business.java
+++ src/main/java/com/decagon/queuepay/models/Business.java
@@ -1,7 +1,6 @@
-package com.decagon.queuepay.models.transaction;
+package com.decagon.queuepay.models;
 
-import com.decagon.queuepay.models.AuditModel;
-import com.decagon.queuepay.models.Business;
+import com.decagon.queuepay.models.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +15,11 @@ import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "transactions")
+@Table(name = "businesses")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Transaction extends AuditModel {
+public class Business extends AuditModel {
 
     @Id
     @GeneratedValue(generator = "UUID")
@@ -28,26 +27,24 @@ public class Transaction extends AuditModel {
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "walletId", nullable = false)
-    private Wallet wallet;
-
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "businessId", nullable = false)
-    private Business business;
-
-    private String customerName;
-
-    private CardType cardType;
+    @JoinColumn(name = "userId", nullable = false)
+    @JsonIgnore
+    private User user;
 
     @NotNull
     @NotBlank
-    private Double amount;
+    private String name;
 
     @NotNull
     @NotBlank
-    private TransactionStatus status;
+    private String logoUrl;
 
     @NotNull
     @NotBlank
-    private TransactionType transactionType;
+    private String CACDocumentUrl;
+
+    @NotNull
+    @NotBlank
+    @Column(columnDefinition="text")
+    private String description;
 }
