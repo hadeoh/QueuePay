fddb613a676a887ac37902bb09594904b0d0b799
Demipo <41957499+Demipo@users.noreply.github.com>
2020-03-06 02:01:48 +0100

    Create AnalyticsController.java

--- src/main/java/com/decagon/queuepay/controller/CashOutController.java
+++ src/main/java/com/decagon/queuepay/controller/CashOutController.java
@@ -1,7 +1,6 @@
 package com.decagon.queuepay.controller;
 
 import com.decagon.queuepay.models.transaction.Transaction;
-import com.decagon.queuepay.payload.Analytics;
 import com.decagon.queuepay.payload.CashOut;
 import com.decagon.queuepay.service.CashOutService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.UUID;
 
 @RestController
 public class CashOutController {
 
-    private CashOutService cashOutService;
     @Autowired
-    public void cashOutService(CashOutService cashOutService) {
-        this.cashOutService = cashOutService;
-    }
+    private CashOutService cashOutService;
 
     @PatchMapping(path = "business/{businessId}/wallet/{walletId}/cashout")
     public ResponseEntity<Transaction> cashOut(
@@ -27,11 +22,6 @@ public class CashOutController {
             @PathVariable(value = "walletId") Integer walletId,
             @Valid @RequestBody CashOut cashOut) throws Exception {
         return new ResponseEntity<Transaction>(cashOutService.cashOut(businessId, walletId, cashOut), HttpStatus.OK);
-    }
-
-    @GetMapping(path = "business/{businessId}/analytics")
-    public ResponseEntity<Analytics> analysis(@PathVariable(value = "businessId") Integer businessId){
-        return new ResponseEntity<Analytics>(cashOutService.getAnalytics(businessId), HttpStatus.OK);
     }
 
 }
